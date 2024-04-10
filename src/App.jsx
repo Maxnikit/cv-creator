2860931fb5aa1105f29f577f0471f4a55f87c608
Maxnikit <max.nikit.03@gmail.com>
2024-04-10 03:30:27 +0300

    Add empty Preview component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import General from "./components/General";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
+import Preview from "./components/Preview";
 import "@fontsource/roboto/500.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -17,6 +18,9 @@ function App() {
           <General />
           <Education />
           <Experience />
+        </div>
+        <div className="preview">
+          <Preview />
         </div>
         <Footer />
       </>
---
+++ src/components/Preview.jsx
@@ -0,0 +1,8 @@
+function Preview({}) {
+  return (
+    <>
+      <h1>Header</h1>
+    </>
+  );
+}
+export default Preview;
