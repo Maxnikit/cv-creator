ef618dbb88e0095474f61e97363d746c3b4d0b62
Maxnikit <max.nikit.03@gmail.com>
2024-04-11 01:08:00 +0300

    lifted all logic from General to App

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,29 @@ import Preview from "./components/Preview";
 import "@fontsource/roboto/500.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useState } from "react";
 // TODO: переписать компоненты. Логика и стейты должны быть в APP, а компоненты меньше
 function App() {
+  // General section
+  const [generalData, setGeneralData] = useState({
+    firstName: "",
+    lastName: "",
+    age: "",
+    email: "",
+    phone: "",
+  });
+  const fullName = generalData.firstName + " " + generalData.lastName;
+  const handleGeneralInfoChange = (e) => {
+    const { name, value } = e.target;
+    setGeneralData({ ...generalData, [name]: value });
+  };
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <>
         <Header />
+        {fullName}
         <div className="main">
-          <General />
+          <General formData={generalData} onChange={handleGeneralInfoChange} />
           <Education />
           <Experience />
         </div>
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,119 +1,56 @@
-import { useState } from "react";
-import Button from "./Button";
 import Input from "./Input";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-function General() {
-  const [isEditing, setIsEditing] = useState(false);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    age: "",
-    email: "",
-    phone: "",
-  });
-  const fullName = formData.firstName + " " + formData.lastName;
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-  const toggleEdit = () => {
-    setIsEditing(!isEditing);
-  };
-  const handleSubmit = () => {
-    saveChanges();
-  };
-  const saveChanges = () => {
-    // TODO: logic to save changes
-    setIsEditing(false);
-  };
-
+function General({ formData, onChange }) {
   return (
     <div className="general">
-      <Accordion defaultExpanded className="accordion">
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          <h2>General</h2>
-        </AccordionSummary>
-        <AccordionDetails>
-          {isEditing && (
-            <form className="card-container" onSubmit={handleSubmit}>
-              <div className="inputNameFields">
-                <Input
-                  type="text"
-                  name="firstName"
-                  label="First name:"
-                  value={formData.firstName}
-                  placeholder="John"
-                  onChange={handleInputChange}
-                  required={true}
-                />
-                <Input
-                  type="text"
-                  name="lastName"
-                  label="Last name:"
-                  value={formData.lastName}
-                  placeholder="Silver"
-                  onChange={handleInputChange}
-                  required={false}
-                />
-              </div>
-              <Input
-                type="number"
-                name="age"
-                label="Age:"
-                value={formData.age}
-                placeholder="33"
-                onChange={handleInputChange}
-                required={true}
-              />
-              <Input
-                type="email"
-                name="email"
-                label="Email:"
-                value={formData.email}
-                placeholder="johnsilv@email.com"
-                onChange={handleInputChange}
-                required={true}
-              />
-              <Input
-                type="tel"
-                name="phone"
-                label="Phone:"
-                value={formData.phone}
-                placeholder="+1 123 456 7890"
-                onChange={handleInputChange}
-                required={true}
-              />
-
-              <Button type="Submit" text="Confirm" />
-            </form>
-          )}
-          {!isEditing && (
-            <div className="card-container">
-              <p className="outputContainer">
-                <strong>Name:</strong> {fullName}
-              </p>
-              <p className="outputContainer">
-                <strong>Age:</strong> {formData.age}
-              </p>
-              <p className="outputContainer">
-                <strong>Email:</strong> {formData.email}
-              </p>
-              <p className="outputContainer">
-                <strong>Phone:</strong> {formData.phone}
-              </p>
-              <Button onClick={toggleEdit} text="Edit" />
-            </div>
-          )}
-        </AccordionDetails>
-      </Accordion>
+      <h2>General</h2>
+      <div className="inputNameFields">
+        <Input
+          type="text"
+          name="firstName"
+          label="First name:"
+          value={formData.firstName}
+          placeholder="John"
+          onChange={onChange}
+          required={true}
+        />
+        <Input
+          type="text"
+          name="lastName"
+          label="Last name:"
+          value={formData.lastName}
+          placeholder="Silver"
+          onChange={onChange}
+          required={false}
+        />
+      </div>
+      <Input
+        type="number"
+        name="age"
+        label="Age:"
+        value={formData.age}
+        placeholder="33"
+        onChange={onChange}
+        required={true}
+      />
+      <Input
+        type="email"
+        name="email"
+        label="Email:"
+        value={formData.email}
+        placeholder="johnsilv@email.com"
+        onChange={onChange}
+        required={true}
+      />
+      <Input
+        type="tel"
+        name="phone"
+        label="Phone:"
+        value={formData.phone}
+        placeholder="+1 123 456 7890"
+        onChange={onChange}
+        required={true}
+      />
     </div>
   );
 }
