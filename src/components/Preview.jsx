22b2700fc45b375e56f5bed79a1eb572df80e89d
Maxnikit <max.nikit.03@gmail.com>
2024-04-13 17:43:05 +0300

    Start working on Preview css

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,11 +1,34 @@
+import "../styles/Preview.css";
 function Preview({ generalData, educationData, experienceData }) {
   const { firstName, lastName, age, email, phone } = generalData;
   return (
     <>
-      <h1>
-        {generalData.firstName} {generalData.lastName}
-      </h1>
-      <div></div>
+      <div className="preview-header">
+        {(firstName || lastName) && (
+          <h1>
+            {firstName} {lastName}
+          </h1>
+        )}
+        <div className="preview-header-personal">
+          {age && <p>Age: {age}</p>}
+          {email && (
+            <p>
+              <i className="fas fa-envelope    "></i>
+              <div className="text">{email}</div>
+            </p>
+          )}
+          {phone && (
+            <p>
+              <i className="fa fa-phone" aria-hidden="true"></i>
+              {phone}
+            </p>
+          )}
+        </div>
+      </div>
+      <div className="preview-main">
+        <div className="preview-education"></div>
+        <div className="preview-experience"></div>
+      </div>
     </>
   );
 }
